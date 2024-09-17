9e1077927f54ef1e0521ab6f588eb2e4d15ca881
boray <bsaygilier33@gmail.com>
2024-09-17 12:41:55 +0300

    feat: add dotenv

--- settlement/src/settlement.ts
+++ settlement/src/settlement.ts
@@ -1,62 +1,42 @@
 import {
   NameService,
-  StateProof,
   offchainState,
   Mina,
   PrivateKey
-} from "../../contracts/build/src/NameService.js";
-import type { SettlementInputs } from "./types.js";
+} from "../../contracts/build/src/NameService";
+import type { CycleConfig, SettlementInputs } from "./types.js";
 import { fetchActions, settle } from "./utils.js";
 
-export { compile, settlementCycle, RETRY_WAIT_MS, MIN_ACTIONS_TO_REDUCE, MAX_RETRIES_BEFORE_REDUCE }
-
-const RETRY_WAIT_MS = Number(process.env.RETRY_WAIT_MS) || 60_000;
-const MIN_ACTIONS_TO_REDUCE = 6;
-const MAX_RETRIES_BEFORE_REDUCE = 100;
-
-type Config = {
-  deployAliases: Record<
-    string,
-    {
-      networkId?: string;
-      url: string;
-      keyPath: string;
-      fee: string;
-      feepayerKeyPath: string;
-      feepayerAlias: string;
-    }
-  >;
-};
+export { compile, settlementCycle }
 
 async function settlementCycle({
-    feePayer,
     nameservice,
     feepayerKey,
     zkAppKey,
-    counter=0
-}: SettlementInputs) {
+    counter=0,
+    config
+}: CycleConfig ) {
     try {
         const actions = await fetchActions(nameservice);
-        let shouldSettle = actions > 0 && (actions > MIN_ACTIONS_TO_REDUCE || counter > MAX_RETRIES_BEFORE_REDUCE);
+        let shouldSettle = actions > 0 && (actions > config.MIN_ACTIONS_TO_REDUCE || counter > config.MAX_RETRIES_BEFORE_REDUCE);
         if(actions === 0) { // If there is nothing to reduce, don't call settle, and don't increment the counter
-            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+            setTimeout(settlementCycle, config.RETRY_WAIT_MS, {nameservice, feepayerKey, zkAppKey, counter});
         } else if (shouldSettle) { // If we should settle the state, then call settle, and reset the cycle to counter = 0
-            await settle({feePayer, nameservice, feepayerKey, zkAppKey});
+            await settle({nameservice, feepayerKey, zkAppKey});
             counter = 0;
-            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+            setTimeout(settlementCycle, config.RETRY_WAIT_MS, {nameservice, feepayerKey, zkAppKey, counter});
         } else { // Otherwise, increment the counter and wait for more actions
             counter++;
-            setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+            setTimeout(settlementCycle, config.RETRY_WAIT_MS, {nameservice, feepayerKey, zkAppKey, counter});
         }
     } catch (error) {
         console.log(error)
         // TODO: If there is an error with the logic, this will just keep looping and catching the error, is there a better approach?
-        setTimeout(settlementCycle, RETRY_WAIT_MS, {feePayer, nameservice, feepayerKey, zkAppKey, counter});
+        setTimeout(settlementCycle, config.RETRY_WAIT_MS, {nameservice, feepayerKey, zkAppKey, counter});
     }
 }
 
-async function compile(zkAppKeysBase58: { privateKey: string; publicKey: string }) {
-    let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
+async function compile(zkAppKey: PrivateKey) {
     let zkAppAddress = zkAppKey.toPublicKey();
 
     const Network = Mina.Network({
